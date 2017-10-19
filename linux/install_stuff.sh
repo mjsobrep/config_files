2f96c39d74dbe89f24e0b79788e09a88dc4009d9
Michael Sobrepera <mjsobrep@live.com>
2017-10-19 03:42:06 -0400

    got started on linux install script

---
+++ linux/install_stuff.sh
@@ -0,0 +1,8 @@
+sudo apt-get update -y && sudo apt-get upgrade -y
+
+sudo apt-get install -y git gedit autojump terminator
+
+cp ~/.bashrc ~/Documents/git/config_files/linux/bashrc-old
+ln -s ~/Documents/git/config_files/linux/bashrc ~/.bashrc
+
+
