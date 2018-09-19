c5ac928c6b6d06193713ff8ef17595d01133eb75
Michael Sobrepera <mjsobrep@live.com>
2018-09-19 11:16:34 -0400

    fixed bashrc moving

--- linux/install_stuff.sh
+++ linux/install_stuff.sh
@@ -2,7 +2,7 @@ sudo apt-get update -y && sudo apt-get upgrade -y
 
 sudo apt-get install -y git gedit autojump terminator
 
-cp ~/.bashrc ~/Documents/git/config_files/linux/bashrc-old
+mv ~/.bashrc ~/Documents/git/config_files/linux/bashrc-old
 ln -s ~/Documents/git/config_files/linux/bashrc ~/.bashrc
 
 
