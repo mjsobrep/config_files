36b186ce1bbb2b464641c1a31455963836fc8a38
Michael Sobrepera <mjsobrep@live.com>
2019-03-20 12:08:20 -0400

    added dependancies for tmux install

--- tmux/install_tmux.sh
+++ tmux/install_tmux.sh
@@ -1,6 +1,7 @@
 sudo apt install libevent-dev
 sudo apt install libncurses5-dev libncursesw5-dev
 
+sudo apt install make autoconf automake pkg-config
 prior=$(pwd)
 cd ~/Downloads
 git clone https://github.com/tmux/tmux.git
