98c45243af6aa1f2955c69a9f776543a13605fc5
Michael Sobrepera <mjsobrep@live.com>
2019-03-20 12:25:35 -0400

    fixed tmux install script to use a release

--- tmux/install_tmux.sh
+++ tmux/install_tmux.sh
@@ -1,12 +1,16 @@
+sudo apt remove tmux
+
 sudo apt install libevent-dev
 sudo apt install libncurses5-dev libncursesw5-dev
 
 sudo apt install make autoconf automake pkg-config
 prior=$(pwd)
 cd ~/Downloads
-git clone https://github.com/tmux/tmux.git
-cd tmux
-sh autogen.sh
+wget https://github.com/tmux/tmux/releases/download/2.8/tmux-2.8.tar.gz
+tar -xzf tmux-2.8.tar.gz
+cd tmux-2.8
 ./configure && make
 sudo make install
 cd $prior
+
+ln -s ~/Documents/git/config_files/tmux/tmux.conf ~/.tmux.conf
