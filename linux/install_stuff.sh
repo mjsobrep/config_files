c288e0affe4d2633e9e38e86d61077bc512e5d5b
Michael Sobrepera <mjsobrep@live.com>
2019-06-03 09:28:40 -0400

    added tex install

--- linux/install_stuff.sh
+++ linux/install_stuff.sh
@@ -9,8 +9,22 @@ sudo add-apt-repository -y ppa:aacebedo/fasd
 sudo apt-get update -y
 sudo apt-get install -y fasd
 
-./zsh/install_zsh.sh 
-./nvim/install_neovim.sh 
+### LATEX ###
+prior=$(pwd)
+cd ~/Downloads
+wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
+tar -xzf install-tl-unx.tar.gz
+rm install-tl-unx.tar.gz
+cd install-tl-*
+read tex_dir <<< $(ls .. | grep install-tl-)
+year=${tex_dir:11:4}
+export TEXLIVE_INSTALL_PREFIX=/home/$USER/texlive
+export TEXLIVE_INSTALL_TEXDIR=/home/$USER/texlive/$year
+./install-tl
+cd $prior
+
+./zsh/install_zsh.sh
+./nvim/install_neovim.sh
 ./tmux/install_tmux.sh
 
 sudo apt autoremove -y
