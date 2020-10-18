26162164f4b5c69f3026670026ab997359c7829e
Michael <mjsobrep@live.com>
2020-10-18 16:16:26 -0400

    Fixed node install to work in one go

--- node/install_node.sh
+++ node/install_node.sh
@@ -1,5 +1,10 @@
 # install node version manager (nvm)
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.1/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 # install latest lts node:
 nvm install --lts
+
+# load npm onto path:
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
