00034350d92f9459b4c9f2a28d2620dc4cef3d0b
Michael Sobrepera <mjsobrep@live.com>
2021-01-28 15:45:50 -0500

    changed brightness and wb a bit

--- peripherals/webcam.sh
+++ peripherals/webcam.sh
@@ -6,7 +6,7 @@ v4l2-ctl -d $DEVICE_NUM --set-ctrl=white_balance_temperature=4200
 #v4l2-ctl -d /dev/video0 --set-ctrl=exposure_auto_priority=0
 #v4l2-ctl -d /dev/video0 --set-ctrl=gain=10
 ##For exposure, valid settings are: 19, 38,77, 155,312, 624, 1250, 2047
-v4l2-ctl -d $DEVICE_NUM --set-ctrl=exposure_absolute=312
+v4l2-ctl -d $DEVICE_NUM --set-ctrl=exposure_absolute=77
 
 #v4l2-ctl -d /dev/video0 --set-ctrl=zoom_absolute=100
 
