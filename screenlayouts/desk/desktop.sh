e2bf817cccd86b706f974743174c4928a94a979e
Michael Sobrepera <mjsobrep@live.com>
2021-07-23 01:00:30 -0400

    updated desktop screen layout

--- screenlayouts/desk/desktop.sh
+++ screenlayouts/desk/desktop.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 gsettings set org.gnome.settings-daemon.plugins.xsettings overrides "[{'Gdk/WindowScalingFactor', <2>}]"
+# HDMI-0 : dell in2020M (old)
+# DP1 vizio
+# dp3 ancor (asus)
+# dp4 dells27 (4k)
 gsettings set org.gnome.desktop.interface scaling-factor 2
-xrandr --output USB-C-0 --off --output HDMI-0 --mode 1600x900 --scale 2x2 --pos 0x0 --rotate left --output DP-5 --mode 1920x1080 --scale 2x2 --pos 6600x764 --rotate normal --output DP-4 --off --output DP-3 --mode 1920x1080 --pos 6600x764 --scale 2x2 --rotate normal --output DP-2 --off --output DP-1 --off --output DP-0 --primary --mode 3840x2160 --scale 1.25x1.25 --pos 1800x324 --rotate normal
+xrandr --output USB-C-0 --off --output HDMI-0 --mode 1600x900 --scale 2x2 --pos 0x0 --rotate left --output DP-3 --mode 1920x1080 --scale 2x2 --pos 6600x764 --rotate normal --output DP-0 --off --output DP-1 --mode 1920x1080 --pos 6600x764 --scale 2x2 --rotate normal --output DP-2 --off --output DP-5 --off --output DP-4 --primary --mode 3840x2160 --scale 1.25x1.25 --pos 1800x324 --rotate normal
