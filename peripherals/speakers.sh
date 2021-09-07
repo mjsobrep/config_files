52b3a56c1d818de1bd42df9db2554d06acbacef1
Michael Sobrepera <mjsobrep@live.com>
2021-09-07 15:02:02 -0700

    added script to flip speakers

---
+++ peripherals/speakers.sh
@@ -0,0 +1,5 @@
+# reverse speakers left-right
+index=$(pacmd list-sinks|grep -B 1 analog-stereo | grep index | tr -d "\n" | tail -c 1)
+echo "remapping for index: $index"
+pacmd load-module module-remap-sink sink_name=reverse-stereo master="$index" channels=2 master_channel_map=front-right,front-left channel_map=front-left,front-right
+pacmd set-default-sink reverse-stereo
