9267b0608cb3183a5af7d5dace0462de3e393ea2
Michael Sobrepera <mjsobrep@live.com>
2021-09-07 15:10:22 -0700

    added volume set to speaker remap

--- peripherals/speakers.sh
+++ peripherals/speakers.sh
@@ -1,5 +1,6 @@
 # reverse speakers left-right
 index=$(pacmd list-sinks|grep -B 1 analog-stereo | grep index | tr -d "\n" | tail -c 1)
 echo "remapping for index: $index"
+pacmd set-sink-volume "$index" 65536
 pacmd load-module module-remap-sink sink_name=reverse-stereo master="$index" channels=2 master_channel_map=front-right,front-left channel_map=front-left,front-right
 pacmd set-default-sink reverse-stereo
